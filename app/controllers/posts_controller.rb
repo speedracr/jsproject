06d83dd4186a09bde3e08a0c060630910cc4b72a
speedracr <jonas@robocomobo.com>
2014-05-30 23:11:50 +0200

    Clean up links in index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(post_params)
+    @post = current_user.posts.build(pin_params)
 
     respond_to do |format|
       if @post.save
